ff0ad01798a45fa10f49646aa46e762bb7b1d155
Norman Clarke <norman@njclarke.com>
2015-01-25 08:42:42 -0800

    Fixed typo

--- haml.info.lua
+++ haml.info.lua
@@ -4,6 +4,6 @@ a(_a, "192.30.252.153", 1200)
 a(_a, "192.30.252.154", 1200)
 
 -- CNAME records
-redirect("www", "http//haml.info/", 1, 1200)
+redirect("www", "http://haml.info/", 1, 1200)
 
 -- cname("blog", "domains.tumblr.com")
