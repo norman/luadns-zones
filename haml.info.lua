b03a718d82e1a6f90d6cf638ac00aa9a1e93ab8f
Norman Clarke <norman@njclarke.com>
2015-01-25 08:40:22 -0800

    Updated to Github pages ips

--- haml.info.lua
+++ haml.info.lua
@@ -1,6 +1,9 @@
 -- A records
-a(_a, "50.116.17.75")
+-- a(_a, "50.116.17.75")
+a(_a, "192.30.252.153", 1200)
+a(_a, "192.30.252.154", 1200)
 
 -- CNAME records
-cname("www", _a)
-cname("blog", "domains.tumblr.com")
+redirect("www", "http//haml.info/", 1, 1200)
+
+-- cname("blog", "domains.tumblr.com")
