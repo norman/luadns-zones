722b25dfcf5cd9a63067dc069d50e00c8fbd2f4a
Norman Clarke <norman@njclarke.com>
2020-07-15 13:40:22 -0300

    Use CNAME instead of A record for haml.info

--- haml.info.lua
+++ haml.info.lua
@@ -1,9 +1,10 @@
 -- A records
 -- a(_a, "50.116.17.75")
-a(_a, "192.30.252.153", 1200)
-a(_a, "192.30.252.154", 1200)
+-- a(_a, "192.30.252.153", 1200)
+-- a(_a, "192.30.252.154", 1200)
 
 -- CNAME records
 redirect("www", "http://haml.info/", 1, 1200)
 
+cname("", "haml.github.io")
 cname("blog", "domains.tumblr.com")
